99b970895ab77d73fa878b4683f7bbf33a23eeae
Jadson Santos <jadsoonbol@gmail.com>
2023-05-31 18:34:56 -0300

    little bug in filter desktop

---
+++ src/tables/TableDepartamentos.js
@@ -0,0 +1,25 @@
+
+
+export const tableDepartamentos = [
+    {
+        id: "RH",
+        nome: "RH"
+    },
+
+    {
+        id: "Diretoria",
+        nome: "Diretoria"
+    },
+
+    {
+        id: "Gerencia",
+        nome: "Gerência"
+    },
+
+    {
+        id: "Contabil",
+        nome: "Contábil"
+    },
+
+]
+
