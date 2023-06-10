124a118e46d555279bc61d9e4aba7d053b529133
unknown <jadsoonbol@gmail.com>
2023-06-09 22:51:30 -0300

    categoria

--- src/components/Visualizar.js
+++ src/components/Visualizar.js
@@ -1,10 +1,10 @@
 import "../styles/Visualizar.css"
 
-export function Visualizar({objeto= null, informacao2, resposta2, informacao3, resposta3, informacao4, resposta4}){
+export function Visualizar({objeto= null, categoria= false, informacao2, resposta2, informacao3, resposta3, informacao4, resposta4}){
 
 
     return(
-        <div className="divBody">
+        <div className={!categoria ? "divBody" : "divBodyCategoria"}>
             <span>
                 <p className="p1">Nome</p><p className="p2">{objeto.nome}</p>
             </span>
