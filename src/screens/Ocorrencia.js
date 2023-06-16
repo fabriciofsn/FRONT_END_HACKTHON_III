d0379a5b067ff96914ee6006f9ef4314cf995a95
Fabricio <banksapk@gmail.com>
2023-06-16 16:23:07 -0300

    modified:   src/screens/Ocorrencia.js

--- src/screens/Ocorrencia.js
+++ src/screens/Ocorrencia.js
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../components/Header";
 import { RiFilter2Fill } from "react-icons/ri";
 
 const Ocorrencia = () => {
+  const [cargo, setCargo] = useState(false);
   return (
-    <Header
-      iconCargo={<RiFilter2Fill />}
-      cargo="Tipos"
-      iconDep={<RiFilter2Fill />}
-      departamento="Departamento"
-    />
+    <>
+      <Header
+        iconCargo={<RiFilter2Fill />}
+        setCargo={setCargo}
+        cargo="Tipos"
+        iconDep={<RiFilter2Fill />}
+        departamento="Departamento"
+      />
+    </>
   );
 };
 
